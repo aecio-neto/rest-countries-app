a6774934fdda4c607195fdbd5135430dd6a792c2
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2024-08-03 05:55:21 -0300

    import Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import { useRef } from "react"
+import { Header } from "./components/Header"
 import '@fontsource/nunito-sans'
 import '@fontsource/nunito-sans/300.css' 
 import '@fontsource/nunito-sans/600.css' 
 import '@fontsource/nunito-sans/800.css' 
 
 export default function App() {
-  return <Timer />
-}
-
-function Timer() {
-  const countRef = useRef(0)
-
-  const increment = () => {
-    countRef.current += 1
-    console.log(countRef.current)
-  }
   return (
-    <div className="p-4 font-nunito-sans">
-      <p className="text-2xl font-bold">Hello</p>
-      <button onClick={increment}>Increment</button>
-    </div>
+    <Header />
   )
 }

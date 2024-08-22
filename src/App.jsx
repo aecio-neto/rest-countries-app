ca50e5d5162b10124fd944ce5b10ad6309af149f
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2024-08-22 10:28:50 -0300

    insert CountriesFeed and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useRef } from "react"
 import { Header } from "./components/Header"
-import '@fontsource/nunito-sans'
-import '@fontsource/nunito-sans/300.css' 
-import '@fontsource/nunito-sans/600.css' 
-import '@fontsource/nunito-sans/800.css' 
+import { CoutriesFeed } from "./components/CoutriesFeed"
+import "@fontsource/nunito-sans"
+import "@fontsource/nunito-sans/300.css"
+import "@fontsource/nunito-sans/600.css"
+import "@fontsource/nunito-sans/800.css"
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <CoutriesFeed />
+    </>
   )
 }

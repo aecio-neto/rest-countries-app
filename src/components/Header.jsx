1622af462337a42afd0c50681ae8f3fc1b6e9a19
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2024-08-24 15:58:55 -0300

    add h1 title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import {ThemeToggle} from "./ThemeToggle"
 export function Header() {
   return (
     <header className="p-6 items-center justify-between flex drop-shadow-md bg-very-light-gray dark:bg-dark-blue dark:text-white">
-      <div className="pl-3 font-bold text-sm">Where in the world</div>
+      <h1 className="pl-3 font-bold text-sm">Where in the world</h1>
       <ThemeToggle />
     </header>
   )

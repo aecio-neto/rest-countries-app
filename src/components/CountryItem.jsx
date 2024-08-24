8fa769d4cda7b2149bf72f720641a4eee24fb070
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2024-08-24 13:36:38 -0300

    pass navigate prop and style country name

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,4 +1,4 @@
-export function CountryItem({ country }) {
+export function CountryItem({ country, navigate }) {
   const {
     name: { common: countryName },
     capital,
@@ -7,15 +7,19 @@ export function CountryItem({ country }) {
     flags: { png: flagUrl, alt },
   } = country;
 
+  const handleCountryClick = () => {
+    navigate(`/country/${countryName}`, countryName);
+  };
+
   return (
-    <a href="#" className="cursor-pointer">
+    <div onClick={handleCountryClick} className="cursor-pointer">
       <div className="md:max-w-80 p-4 pb-10 text-md my-4 font-light shadow-md bg-white dark:bg-dark-blue rounded-md">
         <img src={flagUrl} alt={alt} />
-        <h3 className="font-bold text-lg py-4">Name: {countryName}</h3>
+        <h3 className="font-bold text-lg py-4">{countryName}</h3>
           <p>Region: {continents}</p>
           <p>Population: {population.toLocaleString('en-US')}</p>
           <p>Capital: {capital}</p>
       </div>
-    </a>
+    </div>
   )
 }

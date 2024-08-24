62c968a3804acfd4265b929dcedf7e23fe2f44f3
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2024-08-24 13:33:40 -0300

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { Header } from "./components/Header"
-import { CoutriesFeed } from "./components/CoutriesFeed"
+import { useState, Suspense, useTransition } from "react"
+import CountryPage from "./components/CountryPage"
+import IndexPage from "./components/IndexPage"
+import Layout from "./components/Layout"
+
 import "@fontsource/nunito-sans"
 import "@fontsource/nunito-sans/300.css"
 import "@fontsource/nunito-sans/600.css"
@@ -8,8 +11,32 @@ import "@fontsource/nunito-sans/800.css"
 export default function App() {
   return (
     <>
-      <Header />
-      <CoutriesFeed />
+      <Suspense>
+        <Router />
+      </Suspense>
     </>
   )
+}
+
+function Router() {
+  const [page, setPage] = useState("/")
+  const [isPending, startTransition] = useTransition()
+  const [selectedCountryName, setSelectedCountryName] = useState(null)
+
+  function navigate(url, countryName = null) {
+    startTransition(() => {
+      setSelectedCountryName(countryName)
+      setPage(url)
+    })
+  }
+
+  let content
+  if (page === "/") {
+    content = <IndexPage navigate={navigate} />
+  } else if (page.startsWith("/country/")) {
+    content = (
+      <CountryPage countryName={selectedCountryName} navigate={navigate} />
+    )
+  }
+  return <Layout isPending={isPending}>{content}</Layout>
 }
